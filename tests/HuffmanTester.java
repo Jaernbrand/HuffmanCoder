65e41726d6bf7d845d1e0a8d16df715457b40ac9
Jaernbrand <henrikjarnbrand@gmail.com>
2015-03-04 13:23:21 +0100

    Tre nya test tillagda.

--- tests/HuffmanTester.java
+++ tests/HuffmanTester.java
@@ -1,8 +1,12 @@
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+
 import org.junit.Test;
+
 import java.io.FileInputStream;
 
 import huffman.*;
@@ -36,8 +40,36 @@ public class HuffmanTester {
 			}
 		}
 		return retArr;
-	}
+	} // readByteArray
 	
+	private static String readStringFromFile(String fileName){
+		StringBuilder builder = new StringBuilder();
+		
+		BufferedReader br = null;
+		try{
+			FileReader fr = new FileReader(fileName);
+			br = new BufferedReader(fr);
+			
+			int tmpChar = br.read();
+			while(tmpChar != -1){
+				builder.append( (char)tmpChar );
+			}
+			
+		} catch (IOException e){
+			e.printStackTrace();
+			
+		} finally {
+			try {
+				br.close();
+
+			} catch (IOException ex){
+				ex.printStackTrace();
+			}
+		}
+		return builder.toString();
+	} // readStringFromFile
+	
+	/* Testet verkar inte längre fylla någon funktion.
 	@Test
 	public void testBytesAreCorrect(){
 		String[] args = {"-encode", "testFiles/testInput0.txt", "testFiles/testOutput0.huff"};
@@ -52,4 +84,45 @@ public class HuffmanTester {
 			assertEquals(oracle[i], huffArr[i]);
 		}
 	} // testBytesAreCorrect
+	*/
+	
+	@Test
+	public void testCompressAndDecompressFileTestinput0(){
+		String[] args = {"-encode", "testFiles/testInput0.txt", "testFiles/testOutput0.huff"};
+		Huffman.main(args);
+		
+		String[] args2 = {"-decode", "testFiles/testOutput0.huff", "testFiles/newTestInput0.txt"};
+		Huffman.main(args2);
+		
+		String original = readStringFromFile("testFiles/testInput0.txt");
+		String translated = readStringFromFile("testFiles/newTestInput0.txt");
+		assertEquals(original, translated);
+	}
+	
+	@Test
+	public void testCompressAndDecompressFileTestinput1(){
+		String[] args = {"-encode", "testFiles/testInput1.txt", "testFiles/testOutput1.huff"};
+		Huffman.main(args);
+		
+		String[] args2 = {"-decode", "testFiles/testOutput1.huff", "testFiles/newTestInput1.txt"};
+		Huffman.main(args2);
+		
+		String original = readStringFromFile("testFiles/testInput1.txt");
+		String translated = readStringFromFile("testFiles/newTestInput1.txt");
+		assertEquals(original, translated);
+	}
+	
+	@Test
+	public void testCompressAndDecompressFileCats(){
+		String[] args = {"-encode", "testFiles/Cats.txt", "testFiles/Cats.huff"};
+		Huffman.main(args);
+		
+		String[] args2 = {"-decode", "testFiles/Cats.huff", "testFiles/newCats.txt"};
+		Huffman.main(args2);
+		
+		String original = readStringFromFile("testFiles/Cats.txt");
+		String translated = readStringFromFile("testFiles/newCats.txt");
+		assertEquals(original, translated);
+	}
+	
 } // HuffmanTester
